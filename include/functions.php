0e78bd1e5554efb1e0471b97c86dd76408d3b492
Magnus Enger <magnus@enger.priv.no>
2009-10-01 09:38:23 +0200

    Flytter get_ccl_results_as_xml() fra index.php til include/functions.php

--- include/functions.php
+++ include/functions.php
@@ -368,4 +368,69 @@ function get_type($bib) {
 	}
 }
 
+function get_ccl_results_as_xml($ccl) {
+
+	/*
+	henter funksjonene i catalog.php, catalog.php inneholder
+	funksjoner for å hente ut katalogdata fra z39.50-servere
+	*/
+	require_once 'include/catalog.php';
+	
+	$out = '';
+	
+	/*
+	hvis ikke ccl-parameteren er oppgitt får man en tom XML-struktur
+	tilbake med records som rotnode
+	*/
+	if (!isset($ccl))
+	{
+		
+		$out .= "<records>\n</records>";
+	} 
+	/*
+	hvis ccl-parameteren er satt får man MARCXML basert på ccl-
+	parameteren tilbake
+	*/
+	else
+	{
+		
+		$out .= "<records>\n";
+		/*
+		kjører funksjonen yazCclArray som returnerer en array med
+		MARCXML-data basert på $query. syntaksen er 'normarc'. mot
+		deichmanske kan denne byttes til hvertfall USMARC og MARC21
+		*/
+		$fetch = yazCclArray($ccl, 'normarc');
+		/*
+		henter ut verdien med nøkkelen 'result'. det er her selve
+		dataene ligger lagret. $fetch-arrayen har også en verdi med
+		nøkkel 'hits' som forteller hvor mange records $fetch inneholder
+		*/
+		$data = $fetch['result'];
+		//går gjennom $data-arrayen
+		foreach ($data as $record)
+		{
+			//splitter på nylinjetegn
+			$lines = explode("\n", $record);
+			/*
+			overskriver den første noden i hver record med en
+			'<record>'-node. dette gjør at namespacet blir fjernet
+			og gjør parsing og transformering av XML lettere
+			*/
+			$lines[0] = "<record>";
+			/*
+			samler arrayen $lines til en streng og konverterer til
+			utf-8
+			*/
+			$out .= utf8_encode(implode("\n", $lines));
+		}
+		$out .= "</records>";
+	}
+	
+	// DEBUG echo($out);
+	
+	return $out;
+	
+}
+
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,29 @@
 <?php
+
+/*
+
+Copyright 2009 Pode
+
+This file is part of "Podes reiseplanlegger".
+
+"Podes reiseplanlegger" is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+"Podes reiseplanlegger" is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with "Podes reiseplanlegger".  If not, see <http://www.gnu.org/licenses/>.
+
+Source code available from: 
+http://github.com/pode/reiseplanlegger/
+
+*/
+
 //forteller PHP at feilmeldinger skal vises
 ini_set('display_errors', 1);
 error_reporting(E_ALL|E_STRICT);
@@ -383,70 +408,5 @@ if (isset($_GET['place'])&&!isset($_GET['tittelnr']))
   file_put_contents($benchmarkUrl, $benchmark, FILE_APPEND) or exit("Feil");
 }
 */
-
-function get_ccl_results_as_xml($ccl) {
-
-	/*
-	henter funksjonene i catalog.php, catalog.php inneholder
-	funksjoner for å hente ut katalogdata fra z39.50-servere
-	*/
-	require_once 'include/catalog.php';
-	
-	$out = '';
-	
-	/*
-	hvis ikke ccl-parameteren er oppgitt får man en tom XML-struktur
-	tilbake med records som rotnode
-	*/
-	if (!isset($ccl))
-	{
-		
-		$out .= "<records>\n</records>";
-	} 
-	/*
-	hvis ccl-parameteren er satt får man MARCXML basert på ccl-
-	parameteren tilbake
-	*/
-	else
-	{
-		
-		$out .= "<records>\n";
-		/*
-		kjører funksjonen yazCclArray som returnerer en array med
-		MARCXML-data basert på $query. syntaksen er 'normarc'. mot
-		deichmanske kan denne byttes til hvertfall USMARC og MARC21
-		*/
-		$fetch = yazCclArray($ccl, 'normarc');
-		/*
-		henter ut verdien med nøkkelen 'result'. det er her selve
-		dataene ligger lagret. $fetch-arrayen har også en verdi med
-		nøkkel 'hits' som forteller hvor mange records $fetch inneholder
-		*/
-		$data = $fetch['result'];
-		//går gjennom $data-arrayen
-		foreach ($data as $record)
-		{
-			//splitter på nylinjetegn
-			$lines = explode("\n", $record);
-			/*
-			overskriver den første noden i hver record med en
-			'<record>'-node. dette gjør at namespacet blir fjernet
-			og gjør parsing og transformering av XML lettere
-			*/
-			$lines[0] = "<record>";
-			/*
-			samler arrayen $lines til en streng og konverterer til
-			utf-8
-			*/
-			$out .= utf8_encode(implode("\n", $lines));
-		}
-		$out .= "</records>";
-	}
-	
-	// DEBUG echo($out);
-	
-	return $out;
-	
-}
 
 ?>
